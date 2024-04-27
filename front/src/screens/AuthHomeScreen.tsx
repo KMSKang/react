646d20b7280b33ff5f666a59cc32341e7c04b661
kk920106 <kk920106@naver.com>
2024-04-27 12:03:57 +0900

    first commit

---
+++ front/src/screens/AuthHomeScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {SafeAreaView, Button, StyleSheet, View} from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { AuthStackParamList } from '../navigation/AuthStackNavigator';
+import { authNavigations } from '../constants';
+
+type AuthHomeScreenProps = StackScreenProps<AuthStackParamList, typeof authNavigations.AUTH_HOME>;
+
+// function AuthHomeScreen({navigation}) {
+function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
+    return (
+        <SafeAreaView>
+            <View>
+                <Button title="로그인 화면으로 이동"
+                        // onPress={()=> navigation.navigate('Login')}
+                        onPress={()=> navigation.navigate(authNavigations.LOGIN)}
+                />
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default AuthHomeScreen;
