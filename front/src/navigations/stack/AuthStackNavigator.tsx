932e1ade8501cd9df7edc08a19cdcf59dd912045
kk920106 <kk920106@naver.com>
2024-04-29 11:48:39 +0900

    [2-5] Drawer Navigation

--- front/src/navigations/stack/AuthStackNavigator.tsx
+++ front/src/navigations/stack/AuthStackNavigator.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
-import AuthHomeScreen from '../screens/AuthHomeScreen';
-import LoginScreen from '../screens/LoginScreen';
-import {authNavigations} from '../constants';
+import AuthHomeScreen from '../../screens/AuthHomeScreen';
+import LoginScreen from '../../screens/LoginScreen';
+import {authNavigations} from '../../constants';
 
 export type AuthStackParamList = {
     [authNavigations.AUTH_HOME]: undefined;
     [authNavigations.LOGIN]: undefined;
 }
 
+const Stack = createStackNavigator<AuthStackParamList>();
+
 function AuthStackNavigator() {
-    // const Stack = createStackNavigator();
-    const Stack = createStackNavigator<AuthStackParamList>();
+    // const Stack = createStackNavigator<AuthStackParamList>();
 
     return (
         <Stack.Navigator>
-            {/* <Stack.Screen name="AuthHome"
-                          component={AuthHomeScreen} /> */}
             <Stack.Screen name={authNavigations.AUTH_HOME}
                           component={AuthHomeScreen} />
             <Stack.Screen name={authNavigations.LOGIN}
--- front/src/screens/AuthHomeScreen.tsx
+++ front/src/screens/AuthHomeScreen.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import {SafeAreaView, Button, StyleSheet, View} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
-import { AuthStackParamList } from '../navigation/AuthStackNavigator';
+import { AuthStackParamList } from '../navigations/stack/AuthStackNavigator';
 import { authNavigations } from '../constants';
 
 type AuthHomeScreenProps = StackScreenProps<AuthStackParamList, typeof authNavigations.AUTH_HOME>;
 
-// function AuthHomeScreen({navigation}) {
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
     return (
         <SafeAreaView>
             <View>
                 <Button title="로그인 화면으로 이동"
-                        // onPress={()=> navigation.navigate('Login')}
                         onPress={()=> navigation.navigate(authNavigations.LOGIN)}
                 />
             </View>
