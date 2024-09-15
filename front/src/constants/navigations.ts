38ed3cc5793f636926bf939afad09fd624b6c074
kk920106 <kk920106@naver.com>
2024-09-16 00:26:13 +0900

    [4-1] 현재 위치 연동하기

--- front/src/constants/navigations.ts
+++ front/src/constants/navigations.ts
@@ -1,7 +1,19 @@
+const mainNavigations = {
+    HOME: 'Home',
+    FEED: 'Feed',
+    CALENDAR: 'Calendar',
+} as const;
+  
+
 const authNavigations = {
     AUTH_HOME: 'AuthHome',
     LOGIN: 'Login',
     SIGNUP: 'Signup'
 } as const;
 
-export {authNavigations};
+const mapNavigations = {
+    MAP_HOME: 'MapHome',
+} as const;
+
+// export {authNavigations};
+export {mainNavigations, authNavigations, mapNavigations};
